c3b4f9e08a75d3ad738d642989f18d50f98487ab
Stanislav Medvedev <medvedevsa97@gmail.com>
2023-06-01 00:24:30 +0300

    remove styles

--- extension/src/components/HelloWorld.vue
+++ extension/src/components/HelloWorld.vue
@@ -2,34 +2,15 @@
   <template v-if="isTargetTab">
     <div>
       <h1>{{ msg }} {{}}</h1>
-
-      <div class="card">
+      <div>
         <button type="button" @click="onClick">count is {{ count }}</button>
-        <p>
-          Edit
-          <code>components/HelloWorld.vue</code> to test HMR
-        </p>
       </div>
 
-      <p>
-        Check out
-        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-          >create-vue</a
-        >, the official Vue + Vite starter
-      </p>
-      <p>
-        Install
-        <a href="https://github.com/vuejs/language-tools" target="_blank"
-          >Volar</a
-        >
-        in your IDE for a better DX
-      </p>
-      <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
     </div>
   </template>
   <template v-else>
     <h4>Перейти:</h4>
-    <a :href="targetSiteUrl" target="_blank"
+    <a :href="targetSiteUrl[0]" target="_blank"
       >Федеральная служба по надзору в сфере здравоохранения</a
     >
   </template>
@@ -40,7 +21,12 @@ import { currTabUrl } from "../scripts/inject.js";
 import { ref, onMounted } from "vue";
 
 const count = ref(0);
-const targetSiteUrl = "https://www.roszdravnadzor.gov.ru";
+const targetSiteUrl = [
+  'https://roszdravnadzor.gov.ru',
+  'https://www.roszdravnadzor.gov.ru',
+  'https://gosuslugi.roszdravnadzor.gov.ru',
+]
+
 const isTargetTab = ref(null);
 
 async function onClick() {
@@ -53,16 +39,10 @@ defineProps({
 });
 
 onMounted(async () => {
-  isTargetTab.value = (await currTabUrl()) === targetSiteUrl;
+  const tabUrl = await currTabUrl()
+  isTargetTab.value = targetSiteUrl.includes(tabUrl);
 });
 </script>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-
-h1 {
-  width: 200px;
-}
+<style>
 </style>
